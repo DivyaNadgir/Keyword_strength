537a59a9deb7177ef35ab6519830004b42e8c0d7
DivyaNadgir <divya.nadgir@gmail.com>
2015-01-09 00:14:54 +0530

    Update search_mode.php

--- search_mode.php
+++ search_mode.php
@@ -1,1 +1,27 @@
+<?php
+//Read the command line argument
+$search_key=$argv[1];
 
+//Get the local file contents that is in serialised format
+$recoveredData = file_get_contents('local_array.txt');
+
+//Searching for the keyword after unserialzing the data stored as an array
+foreach(unserialize($recoveredData) as $key=>$value){
+	if(@$value[$search_key]){
+		$result[$key]=$value[$search_key];
+	}
+}
+
+//Showing the search results
+if(@$result){
+	asort($result);
+	$result=array_reverse($result);
+	foreach($result as $each_url=>$key_count){
+		echo "$key_count resutls found in $each_url \n";
+	}
+}else{
+	echo "No results found. \n";
+}
+
+
+?>
